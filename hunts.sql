fb85f50e1b9b0c61d6bb5e1a9468e8131d3f3bf1
mma <mmagraff@gmail.com>
2022-02-17 13:55:47 +0100

    Satt opp grunnlag for database

--- hunts.sql
+++ hunts.sql
@@ -1,5 +1,34 @@
+CREATE TABLE hunts (
+  hunt_id SERIAL NOT NULL PRIMARY KEY,
+  hunt_name VARCHAR(255) NOT NULL,
+  finalMessage VARCHAR(500),
+);
+
+CREATE TABLE locations (
+  location_id SERIAL NOT NULL PRIMARY KEY,
+  hunt_id INT NOT NULL REFERENCES hunts (hunt_id),
+  post_id INT,
+  post_name VARCHAR(255),
+  lat DECIMAL,
+  lng DECIMAL,
+  radius INT,
+  hint VARCHAR(500)
+);
+
+INSERT INTO hunts (hunt_name, finalMessage)
+  VALUES
+    ("Nydalen rundt", "Du har runna Nydalen rundt, bra jobba!"),
+    ("")
 
 
+INSERT INTO locations (hunt_id, post_id, post_name, lat, lng, radius, hint)
+  VALUES
+    (1, 1, "Nydalen Bryggeri", 59.950313170793166, 10.76434599639627, 50, "Lønningspils og quiz hver torsdag"),
+    (1, 2, "BI Nydalen", 59.949107987046006, 10.768578221529944, 100, "Stedet hvor du kan betale for utdanning og alle går i blazer eller dress"),
+    (1, 3, "Fly Chicken Storo", 59.947482697043675, 10.770985011587864, 20, "Fritert guilty pleasure-mat dagen derpå"),
+    (1, 4, "Nydalen Postkontor", 59.94714264963904, 10.769047675381874, 30, "Send en pakke eller fem til en nær venn"),
+    (1, 5, "Kristoffer Aamots bro", 59.94659960206432, 10.765027380498555, 25, "Nærmeste krysningspunkt over Akerselva"),
+    (1, 6, "SAS Brua", 59.94895696213082, 10.765167863515225, 20, "Brua med utsikt til Playa del Nydalen, stedet hvor man bader"); 
 
 
 -- CREATE TABLE users (
@@ -9,7 +38,6 @@
 --   password TEXT
 -- );
 
-
 -- CREATE TABLE tweets (
 --   ID SERIAL PRIMARY KEY,
 --   user_id INT NOT NULL REFERENCES users (ID),
@@ -17,12 +45,10 @@
 --   message VARCHAR(255)
 -- );
 
-
 -- INSERT INTO users (name, username, password)
 --   VALUES
 --     ('Santa Claus', 'santaclaus', '1234'),
 --     ('Donald Trump', 'trump', '4321');
-
 
 -- INSERT INTO tweets (user_id, message)
 --   VALUES
@@ -42,11 +68,6 @@
 -- INNER JOIN users ON
 --   tweets.user_id = users.id
 -- ORDER BY created_at DESC;
-
-
-
-
-
 
 -- -- Select all tweets from one user with user info
 -- SELECT
