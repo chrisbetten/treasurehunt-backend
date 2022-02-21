fe3b323fd2ac1e0197f702f5db50791dbe42cb04
Thobias <thobias.maaoe@gmail.com>
2022-02-21 18:56:42 +0100

    changed res.send to res.end

--- queries.js
+++ queries.js
@@ -98,7 +98,7 @@ async function addNewPosts (request, response) {
         if (error) {
           throw error
         }
-        response.send("Posts added");
+        response.end("Posts added");
       }
     )
   }) 
